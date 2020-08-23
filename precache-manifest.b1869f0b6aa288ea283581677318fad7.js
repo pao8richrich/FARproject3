6d3ea9cbe9b2188020a80c2667ae2267cec1f71a
srico <srico@quantumconnexion.com>
2020-08-23 16:58:45 -0500

    Updates

---
+++ precache-manifest.b1869f0b6aa288ea283581677318fad7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a79724cd43402d1143a0474b8cb09f",
+    "url": "/FARproject3/index.html"
+  },
+  {
+    "revision": "88bc10e0da07ae99b0cc",
+    "url": "/FARproject3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "549f67cbccba8bd360ff",
+    "url": "/FARproject3/static/js/2.f4d943aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FARproject3/static/js/2.f4d943aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bc10e0da07ae99b0cc",
+    "url": "/FARproject3/static/js/main.f4abe9b5.chunk.js"
+  },
+  {
+    "revision": "23ec9b833b154a7914e2",
+    "url": "/FARproject3/static/js/runtime-main.84604774.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FARproject3/static/media/logo.5d5d9eef.svg"
+  }
+]);
